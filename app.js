9a3333c3f6c6085a17f1d4ae43f52a35bcaf582b
Cameron Lay <cameronlay15@gmail.com>
2020-10-06 16:18:47 -0400

    inquirer

--- app.js
+++ app.js
@@ -18,3 +18,38 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
     runApp();
   })
+
+  // select action
+function runApp() {
+    inquirer.prompt({
+      name: "Options",
+      type: "list",
+      choices: ["Add Department", "Add Roles", "Add Employees", "View Departments","View Roles", "View Employees", "Update Roles", "Exit"]
+    }).then(answer => {
+          switch (answer.Options) {
+            case "View Departments":
+            viewDepartments();
+            break;
+            case "View Roles":
+            viewRoles();
+            break;
+            case "View Employees":
+            viewEmployees();
+            break;
+            case "Add Department":
+            addDepartment();
+            break;
+            case "Add Roles":
+            addRoles();
+            break;
+            case "Update Roles":
+            updateRoles();
+            break;
+            case "Add Employees":
+            addEmployees();
+            break;
+            case "Exit":
+            process.exit();
+          }
+        })
+  }
